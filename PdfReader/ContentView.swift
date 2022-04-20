5821444ebb2ddda0b15d16d900c23b81d6738e1d
Mrsazza <shahriarhossain147@gmail.com>
2022-04-21 02:47:32 +0600

    Initial Commit

---
+++ PdfReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PdfReader
+//
+//  Created by Sazza on 21/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PdfReader/PdfReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdfReaderApp.swift
+//  PdfReader
+//
+//  Created by Sazza on 21/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PdfReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
